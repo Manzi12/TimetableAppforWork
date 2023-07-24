cac3001c206c602c8cb8f619d854e6c5479f8d90
Manzi12 <manzijosephis@gmail.com>
2023-07-24 23:00:04 +0100

    Employee manager updated to include fuction for timetable as well

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -34,4 +34,58 @@ public class EmployeeManager {
                     + ", Department: " + employee.getDepartment());
         }
     }
+
+    public void updateTimetabele(){
+        System.out.println("Enter Employee Id to update timetable");
+        int employeeId = Integer.parseInt(scanner.nextLine());
+
+        Employee employeeToUpdate = null;
+        for (Employee employee : employees){
+            if (employee.getId() == employeeId){
+                employeeToUpdate = employee;
+                break;
+            }
+        }
+
+        if (employeeToUpdate == null) {
+            System.out.println("Employee with ID " + employeeId + "not found");
+            return;
+        }
+
+        System.out.println("Enter day of the week eg monday etc");
+        String dayOfweek  = scanner.nextLine();
+
+        System.out.println("Enter shift for " + dayOfweek + ":");
+        String shift = scanner.nextLine();
+
+        employeeToUpdate.getTimetable().setShift(dayOfweek,shift);
+        System.out.println("Timetable updated for employee with ID " + employeeId);
+
+        }
+
+    public void viewTimetable(){
+        System.out.println("Enter employee ID to view Timetable: ");
+        int employeeID = Integer.parseInt(scanner.nextLine());
+
+        Employee employeeToView = null;
+        for (Employee employee : employees){
+            if (employee.getId() == employeeID){
+                employeeToView = employee;
+                break;
+            }
+        }
+
+        if (employeeToView == null){
+            System.out.println("Employee with ID " + employeeID + " not found");
+            return;
+        }
+
+        EmployeeTimetable timetable = employeeToView.getTimetable();
+        System.out.println("Timetable for Employee with ID " + employeeID + ":");
+        for (String dayOfWeek : timetable.getTimetable().keySet()){
+            System.out.println(dayOfWeek + ": " + timetable.getShift(dayOfWeek));
+        }
+    }
+
+
 }
